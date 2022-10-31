2faa68609896db4eac66101ec81c9e59537d23be
DapperNurd <jbenbonus@gmail.com>
2022-10-31 15:14:27 -0700

    attempting to add SFML

--- main.cpp
+++ main.cpp
@@ -8,6 +8,23 @@ int main() {
 
     displayBoard(board);
 
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Window name");
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                // Close window button clicked.
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        // Draw here.
+        window.display();
+    }
+
     return 0;
 
 }
