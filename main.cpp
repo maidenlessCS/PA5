fb7ceba9c47c0b309fdf6ba698fa25113636a891
DapperNurd <jbenbonus@gmail.com>
2022-11-02 15:49:17 -0700

    ADded game sysem added vsync nothing works

--- main.cpp
+++ main.cpp
@@ -8,31 +8,58 @@ int main() {
 
     displayBoard(board);
 
-    sf::RenderWindow window(sf::VideoMode(1024, 812), "Window name");
-    while(window.isOpen())
-    {
-        sf::Event event;
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-            {
-                // Close window button clicked.
-                window.close();
-            }
-            if (event.type == sf::Event::EventType::KeyPressed){
+    // int posX = 0;
 
-                if (event.key.code == sf::Keyboard::Left){
-                    // Do something here
-                }
-                else if (event.key.code == sf::Keyboard::Right){
-                    // Do something here
-                }
+    // sf::RectangleShape square(sf::Vector2f(20, 20));
+    // square.setFillColor(sf::Color(100, 250, 50));
+
+    // sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y), "TETRIS");
+    // while(window.isOpen())
+    // {
+    //     sf::Event event;
+    //     while(window.pollEvent(event))
+    //     {
+    //         if(event.type == sf::Event::Closed)
+    //         {
+    //             // Close window button clicked.
+    //             window.close();
+    //         }
+    //         if (event.type == sf::Event::EventType::KeyPressed){
+
+    //             if (event.key.code == sf::Keyboard::Left){
+    //                 // Do something here
+    //                 if(posX > 0+5) {
+    //                     posX-=5;
+    //                 }
+    //                 square.setPosition(sf::Vector2f(posX, square.getPosition().y));
+    //             }
+    //             else if (event.key.code == sf::Keyboard::Right) {
+    //                 if(posX < WINDOW_SIZE_X-square.getSize().x - 5) {
+    //                     posX+=5;
+    //                 }
+    //                 square.setPosition(sf::Vector2f(posX, square.getPosition().y));
+    //                 // Do something here
+    //             }
                 
-            }
-        }
-        window.clear(sf::Color::Black);
-        // Draw here.
-        window.display();
+    //         }
+    //     }
+    //     window.clear(sf::Color::Black);
+    //     // Draw here.
+    //     window.draw(square);
+    //     window.display();
+    // }
+
+    Game game;
+    
+    const sf::Time TIME_PER_FRAME = sf::seconds(1.f / 60.f);
+    sf::Clock clock; // starts the clock
+    while(!game.isDone())
+    {
+        sf::Time start = clock.restart();
+        game.handleInput();
+        game.update();
+        game.render();
+        sf::sleep(start + TIME_PER_FRAME - clock.getElapsedTime());
     }
 
     return 0;
--- tetris.cpp
+++ tetris.cpp
@@ -1,5 +1,85 @@
 #include "tetris.h"
 
+Game::Game() : mWindow(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y), "TETRIS") {
+    square.setSize(sf::Vector2f(400, 800));
+    square.setOrigin(62.f / 2.f, 62.f / 2.f);
+    mIncrement = sf::Vector2i(4, 4);
+    mIsDone = false;
+}
+
+void Game::handleInput() {
+    sf::Event event;
+    while(mWindow.pollEvent(event))
+    {
+        if(event.type == sf::Event::Closed)
+        {
+            // Close window button clicked.
+            mWindow.close();
+        }
+    }
+}
+
+void Game::update() {
+    sf::Event event;
+        while(mWindow.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                // Close window button clicked.
+                mWindow.close();
+            }
+            if (event.type == sf::Event::EventType::KeyPressed){
+
+                if (event.key.code == sf::Keyboard::Left){
+                    // Do something here
+                    if(posX > 0+5) {
+                        posX-=5;
+                    }
+                    square.setPosition(sf::Vector2f(posX, square.getPosition().y));
+                }
+                else if (event.key.code == sf::Keyboard::Right) {
+                    if(posX < WINDOW_SIZE_X-square.getSize().x - 5) {
+                        posX+=5;
+                    }
+                    square.setPosition(sf::Vector2f(posX, square.getPosition().y));
+                    // Do something here
+                }
+                
+            }
+        }
+}
+
+void Game::moveCherry() {
+    sf::Vector2u winSize = mWindow.getSize();
+    int cherryX = 62;
+    int cherryY = 62;
+    if ((square.getPosition().x + (cherryX / 2) > winSize.x && mIncrement.x > 0) ||
+        (square.getPosition().x - (cherryX / 2) < 0 && mIncrement.x < 0))
+    {
+        // Reverse the direction on X axis
+        mIncrement.x = -mIncrement.x;
+    }
+    if ((square.getPosition().y + (cherryY / 2) > winSize.y && mIncrement.y > 0) ||
+        (square.getPosition().y - (cherryY / 2) < 0 && mIncrement.y < 0))
+    {
+        // Reverse the direction on Y axis.
+        mIncrement.y = -mIncrement.y;
+    }
+    square.setPosition(
+        square.getPosition().x + mIncrement.x,
+        square.getPosition().y + mIncrement.y);
+}
+
+void Game::render() {
+    mWindow.clear(sf::Color::Black);
+    mWindow.draw(square);
+    mWindow.display();
+}
+
+bool Game::isDone() const {
+    return (!mWindow.isOpen() || mIsDone);
+}
+
 void initializeBoard(char board[20][10], char fillChar) {
     for(int i = 0; i < 20; i++) {
         for(int j = 0; j < 10; j++) {
